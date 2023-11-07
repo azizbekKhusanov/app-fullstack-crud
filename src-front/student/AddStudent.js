87d7549e60eb928f9ee380cb695aa6fac8c33641
Azizbek Xusanov <xusanov0511@gmail.com>
2023-11-07 21:40:59 +0500

    First react app

---
+++ src-front/student/AddStudent.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function AddStudent() {
+  let navigate = useNavigate();
+
+  const [student, setStudent] = useState({
+    name: "",
+    email: "",
+    password: "",
+    prePassword: "",
+  });
+
+  const { name, email, password, prePassword } = student;
+
+  const onInputChange = (e) => {
+    setStudent({ ...student, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:9090/api-students/add", student);
+    navigate("/");
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Student add</h2>
+
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type={"email"}
+                className="form-control"
+                placeholder="Enter your e-mail address"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Password" className="form-label">
+                Password
+              </label>
+              <input
+                type={"password"}
+                className="form-control"
+                placeholder="Enter your password"
+                name="password"
+                value={password}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="prePassword" className="form-label">
+                Pre Password
+              </label>
+              <input
+                type={"password"}
+                className="form-control"
+                placeholder="Enter your pre password"
+                name="prePassword"
+                value={prePassword}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/">
+              Cancel
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src-front/student/EditStudent.js
@@ -0,0 +1,108 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+export default function EditStudent() {
+  let navigate = useNavigate();
+
+  const { id } = useParams();   
+
+  const [student, setStudent] = useState({
+    name: "",
+    email: "",
+    password: "",
+    prePassword: "",
+  });
+
+  const { name, email, password, prePassword } = student;
+
+  const onInputChange = (e) => {
+    setStudent({ ...student, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    loadStudent();
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.put(`http://localhost:9090/api-students/edit/${id}`, student);
+    navigate("/");
+  };
+
+  const loadStudent = async () => {
+    const result = await axios.get(`http://localhost:9090/api-students/${id}`);
+    setStudent(result.data);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Edit Student</h2>
+
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your e-mail address"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Password" className="form-label">
+                Password: 
+              </label>
+              <input
+                type={"password"}
+                className="form-control"
+                placeholder="Enter your password"
+                name="password"
+                value={password}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="prePassword" className="form-label">
+                Pre Password
+              </label>
+              <input
+                type={"password"}
+                className="form-control"
+                placeholder="Enter your pre password"
+                name="prePassword"
+                value={prePassword}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/">
+              Cancel
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
